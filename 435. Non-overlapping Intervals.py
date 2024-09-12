d5818bc68862c62d510f91ff0be12d21ced7f7eb
MJ <mwjang628@gmail.com>
2024-09-12 12:34:00 +1000

    435. Non-overlapping Intervals

---
+++ 435. Non-overlapping Intervals.py
@@ -0,0 +1,16 @@
+# 435. Non-overlapping Intervals
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        stack = []
+        count = 0
+        for start, end in intervals:
+            if stack and stack[-1][1] > start:
+                stack[-1][1] = min(stack[-1][1], end)
+                count += 1
+            else:
+                stack.append([start, end])
+
+        return count
