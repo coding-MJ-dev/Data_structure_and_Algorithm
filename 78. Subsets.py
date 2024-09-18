eb40051b113143f82c056761ced22538baefb24b
MJ <mwjang628@gmail.com>
2024-09-18 13:53:14 +1000

    # 78. Subsets

---
+++ 78. Subsets.py
@@ -0,0 +1,17 @@
+# 78. Subsets
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur):
+            if i >= len(nums):
+                res.append(cur[:])
+                return
+
+            dfs(i + 1, cur + [nums[i]])
+            dfs(i + 1, cur)
+
+        dfs(0, [])
+        return res
