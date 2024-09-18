73ced34f8c8225c4810fc079279c7459118a577b
MJ <mwjang628@gmail.com>
2024-09-18 10:52:39 +1000

    287. Find the Duplicate Number

---
+++ 287. Find the Duplicate Number.py
@@ -0,0 +1,10 @@
+# 287. Find the Duplicate Number
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return n
+            seen.add(n)
