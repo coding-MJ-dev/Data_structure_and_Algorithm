6b0bf11d02ec20e8e4d15b30e7008a6339e2b39b
MJ <mwjang628@gmail.com>
2024-10-06 21:34:50 +1100

    14. Longest Common Prefix

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,27 @@
+// 14. Longest Common Prefix
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 1) {
+            return strs[0];
+        }
+
+        string ans {""};
+        sort(strs.begin(), strs.end());
+        int len1 = strs[0].size();
+        int len2 = strs[strs.size()-1].size();
+        int len = min(len1, len2);
+        for (int i = 0; i < len; i++) {
+            if (strs[0][i] == strs[strs.size()-1][i]) {
+                // cout << strs[0][i];
+                ans.push_back(strs[0][i]);
+            } else {
+                break;
+            }
+        }
+        return ans;
+
+    }
+};
