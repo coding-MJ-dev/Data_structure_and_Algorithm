24d3fbd7350f7ffb641a795665adead93cfa277e
MJ <mwjang628@gmail.com>
2024-10-07 11:17:53 +1100

    283. Move Zeroes

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,17 @@
+// 283. Move Zeroes
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i ++;
+            }
+
+        }
+    }
+};
