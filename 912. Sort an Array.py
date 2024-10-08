16b585b4687bb821913ca37a936db043be757695
MJ <mwjang628@gmail.com>
2024-10-09 09:16:53 +1100

    912. Sort an Array

---
+++ 912. Sort an Array.py
@@ -0,0 +1,10 @@
+# 912. Sort an Array
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        heapify(nums)
+        ans = []
+        while nums:
+            ans.append(heappop(nums))
+        return ans
