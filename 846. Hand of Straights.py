30707fc8c4c06565937ba3190098b2d878f383a9
MJ <mwjang628@gmail.com>
2024-10-14 16:25:57 +1100

    846. Hand of Straights

---
+++ 846. Hand of Straights.py
@@ -0,0 +1,30 @@
+# 846. Hand of Straights
+
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        count = collections.Counter(hand)
+        if len(hand) % groupSize != 0:
+            return False
+        number = len(hand) // groupSize
+        heapify(hand)
+        # print(hand)
+        # print(count)
+        for _ in range(number):
+
+            val = heappop(hand)
+            while count[val] == 0:
+                val = heappop(hand)
+            count[val] -= 1
+            # print(count)
+            c = 1
+            # print(val)
+            while c < groupSize:
+                if val + 1 in count and count[val + 1] > 0:
+                    c += 1
+                    count[val + 1] -= 1
+                    val += 1
+                else:
+                    return False
+
+        return True
