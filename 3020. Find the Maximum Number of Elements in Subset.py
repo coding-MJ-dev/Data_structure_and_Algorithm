9a5188f6720e95326340acce9b78d56a66e1577d
coding-MJ-dev <mwjang628@gmail.com>
2024-11-11 17:19:35 +1100

    # 3020. Find the Maximum Number of Elements in Subset

---
+++ 3020. Find the Maximum Number of Elements in Subset.py
@@ -0,0 +1,19 @@
+# 3020. Find the Maximum Number of Elements in Subset
+
+
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        ans = 0
+        for n in count:
+            if n == 1:
+                ans = max(ans, count[n] if count[n] % 2 else count[n] -1)
+            else:
+                cur = 0
+                while n ** 2 in count and count[n] >= 2:
+                   cur += 2
+                   n = n**2
+                cur += 1
+                ans = max(cur, ans)
+        return ans
+
