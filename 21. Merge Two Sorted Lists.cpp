80bedca8383fa76b8f0ad0138628b96a2622bb92
coding-MJ-dev <mwjang628@gmail.com>
2024-12-29 12:10:22 +1100

    // 21. Merge Two Sorted Lists

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -13,37 +13,67 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == NULL && list2 == NULL) {
-            return NULL;
-        }
-        if (list1 == NULL) {
-            return list2;
-        }
-        if (list2 == NULL) {
-            return list1;
-        }
+        ListNode* ans = new ListNode();
+        ListNode* cur = ans;
 
-        ListNode* dummy = new ListNode();
-        ListNode* curr = dummy;
-
-        while (list1 != NULL && list2 != NULL) {
+        while (list1 != nullptr && list2 != nullptr) {
             if (list1->val <= list2->val) {
-                curr->next = list1;
+                cur->next = list1;
+                cur = cur->next;
                 list1 = list1->next;
-            }
-            else {
-                curr->next = list2;
+            } else {
+                cur->next = list2;
+                cur = cur->next;
                 list2 = list2->next;
             }
-            curr = curr->next;
         }
-        if (list1 != NULL) {
-            curr->next = list1;
-        } else {
-            curr->next = list2;
+
+        if (list1 != nullptr) {
+            cur->next = list1;
         }
-        return dummy->next;
+        if (list2 != nullptr) {
+            cur->next = list2;
+        }
+
+        return ans->next;
     }
 };
+
+
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if (list1 == NULL && list2 == NULL) {
+//             return NULL;
+//         }
+//         if (list1 == NULL) {
+//             return list2;
+//         }
+//         if (list2 == NULL) {
+//             return list1;
+//         }
+
+//         ListNode* dummy = new ListNode();
+//         ListNode* curr = dummy;
+
+//         while (list1 != NULL && list2 != NULL) {
+//             if (list1->val <= list2->val) {
+//                 curr->next = list1;
+//                 list1 = list1->next;
+//             }
+//             else {
+//                 curr->next = list2;
+//                 list2 = list2->next;
+//             }
+//             curr = curr->next;
+//         }
+//         if (list1 != NULL) {
+//             curr->next = list1;
+//         } else {
+//             curr->next = list2;
+//         }
+//         return dummy->next;
+//     }
+// };
 
 
