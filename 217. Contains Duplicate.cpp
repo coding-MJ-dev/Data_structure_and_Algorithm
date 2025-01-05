3cd60329d738124c33dbd22fba66124f2ee772a0
coding-MJ-dev <mwjang628@gmail.com>
2025-01-06 09:52:09 +1100

    // 217. Contains Duplicate

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,64 @@
+// 217. Contains Duplicate
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        int n = nums.size();
+        sort(nums.begin(), nums.end()); // Sort the array
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+};
+
+
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end()); // Sort the array
+
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] == nums[i - 1])
+//                 return true;
+//         }
+//         return false;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set<int> seen;
+//         for (int num : nums) {
+//             if (seen.count(num)) {
+//                 return true;
+//             }
+//             seen.insert(num);
+//         }
+//         return false;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_set <int> s;
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (s.find(nums[i]) != s.end())
+//                 return true;
+//             else {
+//                 s.insert(nums[i]);
+//             }
+//         }
+//         return false;
+//     }
+// };
