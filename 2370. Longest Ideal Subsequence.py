0d345cf43eb5a168a73cec5fc416d9afc06d42ff
coding-MJ-dev <mwjang628@gmail.com>
2025-01-20 09:51:55 +1100

    # 2370. Longest Ideal Subsequence

--- 2370. Longest Ideal Subsequence.py
+++ 2370. Longest Ideal Subsequence.py
@@ -3,10 +3,19 @@
 
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
-        dp = [0] * 128
-        ans = 0
+        dp = [0] * 26
         for c in s:
-            idx = ord(c)
-            dp[idx] = max(dp[idx - k: idx + k+1]) + 1
-            ans = max(dp[idx], ans)
-        return ans
+            val = ord(c) - ord('a')
+            l = max(0, val - k)
+            r = min(25, val + k)
+            dp[val] = max(dp[val], max(dp[l:r+1])+ 1)
+        return max(dp)
+
+
+        # dp = [0] * 128
+        # ans = 0
+        # for c in s:
+        #     idx = ord(c)
+        #     dp[idx] = max(dp[idx - k: idx + k+1]) + 1
+        #     ans = max(dp[idx], ans)
+        # return ans
