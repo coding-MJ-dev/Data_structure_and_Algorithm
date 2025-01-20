a0bdbbb9b64ef86696ddff459f82479700aacbea
coding-MJ-dev <mwjang628@gmail.com>
2025-01-21 08:42:13 +1100

    # 652. Find Duplicate Subtrees

--- 652. Find Duplicate Subtrees.py
+++ 652. Find Duplicate Subtrees.py
@@ -8,19 +8,43 @@
 #         self.right = right
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
-        dp = defaultdict(list)
         ans = []
-        
-        def dfs(node):
-            if not node:
-                return "N"
+        dp = defaultdict(int)
+
+        def dfs(root):
+            if not root:
+                return '#'
             
-            key = ','.join([str(node.val), dfs(node.left), dfs(node.right)])
-            if len(dp[key]) == 1:
-                ans.append(node)
-            dp[key].append(node)
-            return key
-        
+            val = f"{root.val},{dfs(root.left)},{dfs(root.right)}"
+            dp[val] += 1
+            if dp[val] == 2:
+                ans.append(root)
+            return val
+
         dfs(root)
+        # print(dp)
         return ans
+
+
+
+
+
+
+
+
+        # dp = defaultdict(list)
+        # ans = []
+        
+        # def dfs(node):
+        #     if not node:
+        #         return "N"
+            
+        #     key = ','.join([str(node.val), dfs(node.left), dfs(node.right)])
+        #     if len(dp[key]) == 1:
+        #         ans.append(node)
+        #     dp[key].append(node)
+        #     return key
+        
+        # dfs(root)
+        # return ans
             
