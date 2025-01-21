0609d0f265de79882be89c99276577c0d56f7b66
coding-MJ-dev <mwjang628@gmail.com>
2025-01-22 08:34:09 +1100

    # 894. All Possible Full Binary Trees

--- 894. All Possible Full Binary Trees.py
+++ 894. All Possible Full Binary Trees.py
@@ -8,22 +8,41 @@
 #         self.right = right
 class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-        dp = {0: [], 1: [TreeNode(0)]}
+        dp = {0: [], 1: [TreeNode()]}
 
-        def dfs(n):
-            if n in dp:
-                return dp[n]
-            
-            ans = []
-            for root in range(2, n+1):
-                left = root - 1
-                right = n - root
-                leftTrees = dfs(left)
-                rightTrees = dfs(right)
-                for lt in leftTrees:
-                    for rt in rightTrees:
-                        ans.append(TreeNode(0, lt, rt))
-            dp[n] = ans
-            return ans
+        for nodes in range(2, n+1):
+            val = []
+            for rootnode in range(1, nodes):
+                left = dp[rootnode - 1]
+                right = dp[nodes - rootnode]
+                for lt in left:
+                    for rt in right:
+                        val.append(TreeNode(0, lt, rt))
+            dp[nodes] = val
         
-        return dfs(n)
+        # print(dp)
+        return dp[n]
+    
+
+
+    
+
+        # dp = {0: [], 1: [TreeNode(0)]}
+
+        # def dfs(n):
+        #     if n in dp:
+        #         return dp[n]
+            
+        #     ans = []
+        #     for root in range(2, n+1):
+        #         left = root - 1
+        #         right = n - root
+        #         leftTrees = dfs(left)
+        #         rightTrees = dfs(right)
+        #         for lt in leftTrees:
+        #             for rt in rightTrees:
+        #                 ans.append(TreeNode(0, lt, rt))
+        #     dp[n] = ans
+        #     return ans
+        
+        # return dfs(n)
