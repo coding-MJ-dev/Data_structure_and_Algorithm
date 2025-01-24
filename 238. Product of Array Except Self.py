825a263033614c82eac306eb44831adb52e0208f
coding-MJ-dev <mwjang628@gmail.com>
2025-01-25 08:22:11 +1100

    # 238. Product of Array Except Self

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -3,22 +3,43 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        check = nums.count(0)
-        if check >= 2:
-            return [0] * len(nums)
+        n = len(nums)
+        ans = [1] * n
+        
+        left_product = 1
+        for i in range(n):
+            ans[i] = left_product
+            left_product *= nums[i]
 
-        mul = 1
-        for n in nums:
-            if n != 0:
-                mul *= n
-
-        ans = []
-        for n in nums:
-            if check == 0:
-                ans.append(mul // n)
-            elif check == 1:
-                if n != 0:
-                    ans.append(0)
-                else:
-                    ans.append(mul)
+        right_product = 1
+        for i in range(n-1, -1, -1):
+            ans[i] *= right_product
+            right_product *= nums[i]
+        
         return ans
+
+
+
+
+
+
+
+        # check = nums.count(0)
+        # if check >= 2:
+        #     return [0] * len(nums)
+
+        # mul = 1
+        # for n in nums:
+        #     if n != 0:
+        #         mul *= n
+
+        # ans = []
+        # for n in nums:
+        #     if check == 0:
+        #         ans.append(mul // n)
+        #     elif check == 1:
+        #         if n != 0:
+        #             ans.append(0)
+        #         else:
+        #             ans.append(mul)
+        # return ans
--- 978. Longest Turbulent Subarray.py
+++ 978. Longest Turbulent Subarray.py
@@ -17,3 +17,21 @@ class Solution:
             ans = max(ans, incresing, decresing)
         
         return ans
+
+
+
+        # compare = 0
+        # ans = 1
+        # cur = 1
+        # for i in range(1, len(arr)):
+        #     if (arr[i] - arr[i-1] < 0 and compare >= 0) or (arr[i] - arr[i-1] > 0 and compare <= 0):
+        #         # print(arr[i])
+        #         compare = arr[i] - arr[i-1]
+        #         cur += 1
+        #         ans = max(ans, cur)
+        #     else:
+        #         # cur = 1
+        #         # compare = 0
+        #         cur = 2 if arr[i] != arr[i-1] else 1
+        #         compare = arr[i] - arr[i-1]
+        # return ans
