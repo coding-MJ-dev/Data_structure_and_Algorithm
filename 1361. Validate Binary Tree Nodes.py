3072bd61617363e21a6ed79319cc3c171dc0d026
coding-MJ-dev <mwjang628@gmail.com>
2025-02-10 08:37:11 +1100

    # 1361. Validate Binary Tree Nodes

--- 1361. Validate Binary Tree Nodes.py
+++ 1361. Validate Binary Tree Nodes.py
@@ -2,33 +2,67 @@
 
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
-        # no cycle, all nodes should be connected, 1 root
+        # bfs solution
+        children = set(leftChild + rightChild)
+        children.discard(-1)
 
-        child = set(leftChild + rightChild)
-        child.discard(-1)
-        if len(child) != n-1:
+        if len(children) != n-1:
             return False
         
-        root = -1
+        root = None
         for i in range(n):
-            if i not in child:
+            if i not in children:
                 root = i
                 break
         
-        #find cycle
         visit = set()
-        def dfs(node):
-            if node == -1:
-                return True
-            if node in visit:
+        q = deque([root])
+        visit.add(root)
+        
+        while q:
+            node = q.popleft()
+            l = leftChild[node]
+            r = rightChild[node]
+            if (l in visit) or (r in visit):
                 return False
 
-            visit.add(node)
+            if l != -1:
+                visit.add(l)
+                q.append(l)
+            if r != -1:
+                visit.add(r)
+                q.append(r)
+        return True if len(visit) == n else False
+    
 
-            if leftChild[node] == -1 and rightChild[node] == -1:
-                return True
+        # dfs solution
+        # no cycle, all nodes should be connected, 1 root
 
-            return dfs(leftChild[node]) and dfs(rightChild[node])
+        # child = set(leftChild + rightChild)
+        # child.discard(-1)
+        # if len(child) != n-1:
+        #     return False
+        
+        # root = -1
+        # for i in range(n):
+        #     if i not in child:
+        #         root = i
+        #         break
+        
+        # #find cycle
+        # visit = set()
+        # def dfs(node):
+        #     if node == -1:
+        #         return True
+        #     if node in visit:
+        #         return False
+
+        #     visit.add(node)
+
+        #     if leftChild[node] == -1 and rightChild[node] == -1:
+        #         return True
+
+        #     return dfs(leftChild[node]) and dfs(rightChild[node])
             
         
-        return dfs(root) and len(visit) == n
+        # return dfs(root) and len(visit) == n
