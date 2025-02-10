c0436035d46da2c7031745ea2c48b666a6c43c08
coding-MJ-dev <mwjang628@gmail.com>
2025-02-11 08:53:02 +1100

    # 2104. Sum of Subarray Ranges

--- 2104. Sum of Subarray Ranges.py
+++ 2104. Sum of Subarray Ranges.py
@@ -2,25 +2,48 @@
 
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        small_sum = 0
-        large_sum = 0
-        
-        stack = [-1]
-        nums.append(-float('inf'))
-        for i, n in enumerate(nums):
-            while nums[stack[-1]] > n:
-                popInd = stack.pop()
-                val = nums[popInd]
-                small_sum += (popInd - stack[-1]) * (i - popInd) * val
-            stack.append(i)
+        n, answer = len(nums), 0
+        stack = []
 
-        stack = [-1]
-        nums[-1] = float('inf')
-        for i, n in enumerate(nums):
-            while nums[stack[-1]] < n:
-                popInd = stack.pop()
-                val = nums[popInd]
-                large_sum += (popInd - stack[-1]) * (i - popInd) * val
-            stack.append(i)
+        #
+        for right in range(n+1):
+            while stack and (right == n or nums[stack[-1]] >= nums[right]):
+                mid = stack.pop()
+                left = -1 if not stack else stack[-1]
+                answer -= nums[mid] * (mid - left) * (right - mid)
+            stack.append(right)
         
-        return large_sum - small_sum
+        #
+        stack.clear()
+        for right in range(n+1):
+            while stack and (right == n or nums[stack[-1]] <= nums[right]):
+                mid = stack.pop()
+                left = -1 if not stack else stack[-1]
+                answer += nums[mid] * (mid-left) * (right-mid)
+            stack.append(right)
+
+        return answer
+
+
+        # small_sum = 0
+        # large_sum = 0
+        
+        # stack = [-1]
+        # nums.append(-float('inf'))
+        # for i, n in enumerate(nums):
+        #     while nums[stack[-1]] > n:
+        #         popInd = stack.pop()
+        #         val = nums[popInd]
+        #         small_sum += (popInd - stack[-1]) * (i - popInd) * val
+        #     stack.append(i)
+
+        # stack = [-1]
+        # nums[-1] = float('inf')
+        # for i, n in enumerate(nums):
+        #     while nums[stack[-1]] < n:
+        #         popInd = stack.pop()
+        #         val = nums[popInd]
+        #         large_sum += (popInd - stack[-1]) * (i - popInd) * val
+        #     stack.append(i)
+        
+        # return large_sum - small_sum
