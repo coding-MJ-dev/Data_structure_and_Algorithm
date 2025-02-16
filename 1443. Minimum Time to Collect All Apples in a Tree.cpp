43ef2e2d940fa98db7c8e20cb73b26bba3d19b88
coding-MJ-dev <mwjang628@gmail.com>
2025-02-17 08:27:37 +1100

    //1443. Minimum Time to Collect All Apples in a Tree

--- 1443. Minimum Time to Collect All Apples in a Tree.cpp
+++ 1443. Minimum Time to Collect All Apples in a Tree.cpp
@@ -1,9 +1,12 @@
 //1443. Minimum Time to Collect All Apples in a Tree
+#include <vector>
+#include <unordered_map>
+
 
 class Solution {
 public:
-    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
-        unordered_map<int, vector<int>> adj;
+    int minTime(int n, std::vector<std::vector<int>>& edges, std::vector<bool>& hasApple) {
+        std::unordered_map<int, std::vector<int>> adj;
 
         for (auto& edge : edges){
             int s = edge[0], e = edge[1];
@@ -14,7 +17,7 @@ public:
     }
 
 private:
-    int dfs(int node, int pre, unordered_map<int, vector<int>>& adj, vector<bool>& hasApple) {
+    int dfs(int node, int pre, std::unordered_map<int, std::vector<int>>& adj, std::vector<bool>& hasApple) {
         int time = 0;
         for (int nei : adj[node]) {
             if (nei == pre) continue;
