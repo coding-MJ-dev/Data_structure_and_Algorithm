b910afbda35d12552be77275d45bdf4bd66fd79c
coding-MJ-dev <mwjang628@gmail.com>
2025-03-11 10:35:16 +1100

    # 338. Counting Bits

--- 338. Counting Bits.py
+++ 338. Counting Bits.py
@@ -1,22 +1,42 @@
+# 338. Counting Bits
+
+
 class Solution:
     def countBits(self, n: int) -> list[int]:
         if n == 0:
             return [0]
         if n == 1:
-            return [0, 1]
+            return [0,1]
 
-        pivot = 1
-        ans = [0] * (n + 1)
-        ans[1] = 1
-        i = 2
-        while i < n + 1:
-            if i / 2 == pivot:
-                pivot = i
-            ans[i] = 1 + ans[i - pivot]
-            i += 1
-
+        ans = [0, 1]
+        for i in range(2, n+1):
+            ans.append(ans[i//2] + i%2)
+        
         return ans
 
+# 
+# 
+# 
+# 
+# 
+# 
+#         if n == 0:
+#             return [0]
+#         if n == 1:
+#             return [0, 1]
 
-sol = Solution()
-print(sol.countBits(5))
+#         pivot = 1
+#         ans = [0] * (n + 1)
+#         ans[1] = 1
+#         i = 2
+#         while i < n + 1:
+#             if i / 2 == pivot:
+#                 pivot = i
+#             ans[i] = 1 + ans[i - pivot]
+#             i += 1
+
+#         return ans
+
+
+# sol = Solution()
+# print(sol.countBits(5))
