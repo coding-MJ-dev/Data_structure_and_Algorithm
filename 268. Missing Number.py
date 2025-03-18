c130872da86f9f08583bef49a5f97484707afd0d
coding-MJ-dev <mwjang628@gmail.com>
2025-03-19 08:36:36 +1100

    # 268. Missing Number

---
+++ 268. Missing Number.py
@@ -0,0 +1,10 @@
+# 268. Missing Number
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums.sort()
+        for i in range(n):
+            if nums[i] != i:
+                return i
+        return n
